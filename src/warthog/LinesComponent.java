8ec99c7e14a21d76a0ee46ed69a19bacad078938
Vishesh Javangula <visheshj123@gwmail.gwu.edu>
2019-03-18 11:22:04 -0400

    Update LinesComponent.java

--- src/warthog/LinesComponent.java
+++ src/warthog/LinesComponent.java
@@ -1,1 +1,89 @@
+mport java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.Line2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.LinkedList;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class LinesComponent extends JComponent{
+   // WartHogMethods points = new WartHogMethods();
+   // double x1val = points.x1;
+  //  System.out.println("The value of x1 in linescomponent is: " + x1val);
+
+    public class Line {
+        WartHogMethods points = new WartHogMethods();
+        double x1;
+        double y1;
+        double x2;
+        double y2;
+        public Color color;
+        float stroke;
+
+
+
+        public Line(double x1, double y1, double x2, double y2, Color color, float stroke) {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+            this.color = color;
+            this.stroke = stroke;
+        }
+    }
+
+
+    private final LinkedList<Line> lines = new LinkedList<Line>();
+
+    public void addLine(double x1, double x2, double x3, double x4, float stroke) {
+        addLine(x1, x2, x3, x4, Color.red, stroke);
+    }
+
+    public void addLine(double x1, double x2, double x3, double x4, Color color, float stroke) {
+        lines.add(new Line(x1,x2,x3,x4, color, stroke));
+        repaint();
+    }
+
+    public void clearLines() {
+        lines.clear();
+        repaint();
+    }
+
+    @Override
+    //takes in image to draw on top off, also goes through each line to draw
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        super.paintComponent(g2);
+        try {
+
+            File f = new File("/Users/visheshj/Desktop/rsz_hufflepuff.jpg");
+            BufferedImage img = ImageIO.read(f);
+            g2.drawImage(img, 0,0, this);
+            for (Line line : lines) {
+
+                g2.setColor(Color.red);
+                g2.setStroke(new BasicStroke(line.stroke));
+                g2.draw(new Line2D.Double(line.x1, line.y1, line.x2, line.y2));
+                //g.drawLine(line.x1, line.y1, line.x2, line.y2);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+
+}
