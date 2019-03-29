c762ef0267dedab95804f42b74b068cc82870b87
Vishesh Javangula <visheshj123@gwmail.gwu.edu>
2019-03-29 15:06:36 -0400

    Update Gradient.java

--- src/warthog/Gradient.java
+++ src/warthog/Gradient.java
@@ -1,5 +1,6 @@
 
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.HashMap;
 import javax.imageio.ImageIO;
 import java.io.File;
@@ -21,200 +22,64 @@ public class Gradient {
     int n = 0;
     int hashnum = 0;
     ArrayList<HashMap> Histolist = new ArrayList<HashMap>();
-    BufferedImage img = null; //allows import of image with each pixel having coordinate point, starting with (0,0) at top left
+    String img = null; //allows import of image with each pixel having coordinate point, starting with (0,0) at top left
     int blocksize = 16;
     double [][] gArray;
     double [][]DirArray;
+    BufferedImage IMG;
 
 
-    public static final String IMG = "/Users/visheshj/Desktop/rsz_hufflepuff.jpg";
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Gradient gradient = new Gradient();
         System.out.println(gradient.blocksize);
 
-       WartHogMethods wartfunctions = new WartHogMethods();
-
-       // BufferedImage img = null; //allows import of image with each pixel having coordinate point, starting with (0,0) at top left
-        File f = null;
-        try {
-            f = new File("/Users/visheshj/Desktop/rsz_hufflepuff.jpg");
-            gradient.img = ImageIO.read(f);
+        WartHogMethods wartfunctions = new WartHogMethods();
 
 
-            //will store every pixels rgb value in 3 columns, this is [y-axis][x-axis]
-            int[][] pixelData = new int [gradient.img.getHeight()][gradient.img.getWidth()];
-            int rgb_avg;
-            int counter = 0;
-            gradient.gArray = new double [gradient.img.getHeight()][gradient.img.getWidth()];
-            gradient.DirArray = new double[gradient.img.getHeight()][gradient.img.getWidth()];
+        JFrame Pathframe = new JFrame();
+        Pathframe.pack();
+        JTextField imgpath = new JTextField();
+        imgpath.setPreferredSize( new Dimension( 200, 24 ));
+        JButton imgEnter = new JButton("Enter");
 
-            System.out.println("Height of image is " + gradient.img.getHeight());
-            System.out.println("Width of image is " + gradient.img.getWidth());
+        Pathframe.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
+        JPanel pathPanel = new JPanel();
+        pathPanel.add(imgEnter);
+        pathPanel.add(imgpath);
+        Pathframe.getContentPane().add(pathPanel);
 
-            //creates greyscale to capture intensity values, populates pixelData array
-            BufferedImage greyScale = wartfunctions.getPixelData(gradient.img, pixelData);
+        Pathframe.setVisible(true);
 
+        imgEnter.addActionListener(new ActionListener() {
 
+                                       @Override
+                                       public void actionPerformed(ActionEvent e) {
+                                           gradient.img = imgpath.getText();
+                                           mainthread m = new mainthread(gradient);
+                                           m.start();
 
-            //calculates gradient and magnitude array, normalizes angles to (0,180)
-            wartfunctions.matrixMultiply(pixelData, gradient.img, gradient.gArray, gradient.DirArray);
+                                       }
 
-           // System.out.print(Histolist.get(0).values());
+                                   });
 
-
-
-        }catch (IOException e){
-            e.printStackTrace();
+        /*while(gradient.img != null){
+            mainthread m = new mainthread();
+            m.start();
         }
+
+
+            System.out.println("You must enter a file path");*/
+
+
+
+        // BufferedImage img = null; //allows import of image with each pixel having coordinate point, starting with (0,0) at top left
+
 
 //imported Jframe
 
-        JFrame ButtonFrame = new JFrame();
-        JFrame ImageFrame = new JFrame();
-
-        JTextField blockSize = new JTextField();
-        blockSize.setBounds(200, 200, 50, 10);
-
-        JButton blockEnter = new JButton("Enter");
-        blockEnter.setBounds(200, 400, 50, 50);
-
-
-        ButtonFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        ImageFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
-
-        final LinesComponent comp = new LinesComponent();
-        final LinesComponent imagecomp = new LinesComponent();
-
-        comp.setPreferredSize(new Dimension(gradient.img.getWidth(), gradient.img.getHeight()));
-        imagecomp.setPreferredSize(new Dimension(gradient.img.getWidth(), gradient.img.getHeight()));
-
-        ButtonFrame.getContentPane().add(comp, BorderLayout.CENTER);
-        ImageFrame.getContentPane().add(imagecomp, BorderLayout.CENTER);
-
-
-        JPanel buttonsPanel = new JPanel();
-        JPanel imagePanel = new JPanel();
-
-
-        JButton newLineButton = new JButton("New Line");
-        JButton resetButton = new JButton("Reset");
-        JButton NextButton = new JButton("Next Histogram");
-        JButton clearButton = new JButton("Clear");
-
-        buttonsPanel.add(newLineButton);
-        buttonsPanel.add(clearButton);
-        buttonsPanel.add(NextButton);
-        buttonsPanel.add(resetButton);
-        buttonsPanel.add(blockEnter);
-
-        ButtonFrame.add(blockSize, BorderLayout.CENTER);
-
-
-        ButtonFrame.getContentPane().add(buttonsPanel, BorderLayout.SOUTH);
-        newLineButton.addActionListener(new ActionListener() {
-
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                 float stroke;
-               // float stroke = 0.003F;
-                //run function to get next vector values from a specific histogram
-                if (gradient.n < 180) {
-                    stroke = (float) ((wartfunctions.drawVector(gradient.Histolist.get(gradient.hashnum), gradient.n))/1000);
-
-                    //determines the x1,x2,y1,y2 coordinates
-                    wartfunctions.drawVector(gradient.Histolist.get(gradient.hashnum), gradient.n);
-
-                    double x1 = wartfunctions.x1;
-                    double y1 = wartfunctions.y1;
-                    double x2 = wartfunctions.x2;
-                    double y2 = wartfunctions.y2;
-                    //Color randomColor = new Color((float) Math.random(), (float) Math.random(), (float) Math.random());
-                    //Integer stroke = gradient.Histolist.get(gradient.hashnum).get(gradient.n);
-                    imagecomp.addLine(x1, y1, x2, y2, Color.red, stroke);
-                    gradient.n += 20;
-                }
-                else {
-                    double x1 = 0.0;
-                    double y1 = 0.0;
-                    double x2 = 0.0;
-                    double y2 = 0.0;
-                    stroke = 0.0F;
-                    Color randomColor = new Color((float) Math.random(), (float) Math.random(), (float) Math.random());
-                    imagecomp.addLine(x1, y1, x2, y2, randomColor, stroke);
-                }
-            }
-        });
-        resetButton.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                imagecomp.clearLines();
-                gradient.n = 0;
-                gradient.hashnum = 0;
-            }
-        });
-
-        clearButton.addActionListener(new ActionListener() {
-
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                imagecomp.clearLines();
-
-            }
-        });
-
-        NextButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                gradient.n = 0;
-                gradient.hashnum++;
-                if ((wartfunctions.x1 + gradient.blocksize) < (gradient.img.getWidth())){ //move 16 to get to center of new block
-                    wartfunctions.x1 += gradient.blocksize;
-                } else{ //if x1 cannot move anymore, then go to next row
-                    wartfunctions.x1 = gradient.blocksize/2; //should reset back to left-hand center
-                    wartfunctions.y1 +=gradient.blocksize;
-                }
-
-                //needs to shift where histogram is printed
-
-
-
-            }
-        });
-
-        blockEnter.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e) {
-                String s1 = blockSize.getText();
-
-                gradient.blocksize = Integer.parseInt(s1);
-                System.out.println("Block size: " + gradient.blocksize);
-
-                HashMap<Integer, Integer> HistoMap = new HashMap<Integer, Integer>();
-                gradient.Histolist = wartfunctions.createHistoList(HistoMap, gradient.gArray, gradient.DirArray, gradient.img.getWidth(), gradient.img.getHeight(), gradient.blocksize);
-
-
-                //prints out all histograms in arraylist
-                for(int i=0; i<gradient.Histolist.size(); i++){
-                    //System.out.print(Histolist.get(i).values());
-                    wartfunctions.printStars(gradient.Histolist.get(i));
-                }
-
-
-            }
-
-        });
-        ButtonFrame.pack();
-        ImageFrame.pack();
-        ButtonFrame.setVisible(true);
-        ImageFrame.setVisible(true);
-
-
     }
-
 }
+
+
