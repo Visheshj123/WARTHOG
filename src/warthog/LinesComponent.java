bf344eb55eb6d0ab0b0a227076033325c4ca3606
Vishesh Javangula <visheshj123@gwmail.gwu.edu>
2019-04-18 21:23:11 -0400

    Update LinesComponent.java

--- src/warthog/LinesComponent.java
+++ src/warthog/LinesComponent.java
@@ -18,6 +18,10 @@ import javax.swing.JPanel;
 import java.awt.*;
 
 public class LinesComponent extends JComponent{
+    Gradient gradient = new Gradient();
+    LinesComponent(Gradient gradient){
+        this.gradient = gradient;
+    }
    // WartHogMethods points = new WartHogMethods();
    // double x1val = points.x1;
   //  System.out.println("The value of x1 in linescomponent is: " + x1val);
@@ -28,16 +32,20 @@ public class LinesComponent extends JComponent{
         double y1;
         double x2;
         double y2;
+        double x3;
+        double y3;
         public Color color;
         float stroke;
 
 
 
-        public Line(double x1, double y1, double x2, double y2, Color color, float stroke) {
+        public Line(double x1, double y1, double x2, double y2, double x3, double y3, Color color, float stroke) {
             this.x1 = x1;
             this.y1 = y1;
             this.x2 = x2;
             this.y2 = y2;
+            this.x3 = x3;
+            this.y3 = y3;
             this.color = color;
             this.stroke = stroke;
         }
@@ -46,12 +54,12 @@ public class LinesComponent extends JComponent{
 
     private final LinkedList<Line> lines = new LinkedList<Line>();
 
-    public void addLine(double x1, double x2, double x3, double x4, float stroke) {
-        addLine(x1, x2, x3, x4, Color.red, stroke);
+    public void addLine(double x1, double y1, double x2, double y2, double x3, double y3, float stroke) {
+        addLine(x1, y1, x2, y2, x3, y3, Color.red, stroke);
     }
 
-    public void addLine(double x1, double x2, double x3, double x4, Color color, float stroke) {
-        lines.add(new Line(x1,x2,x3,x4, color, stroke));
+    public void addLine(double x1, double y1, double x2, double y2, double x3, double y3, Color color, float stroke) {
+        lines.add(new Line(x1,y1,x2,y2, x3, y3, color, stroke));
         repaint();
     }
 
@@ -67,7 +75,7 @@ public class LinesComponent extends JComponent{
         super.paintComponent(g2);
         try {
 
-            File f = new File("/Users/visheshj/Desktop/rsz_hufflepuff.jpg");
+            File f = new File(gradient.img);
             BufferedImage img = ImageIO.read(f);
             g2.drawImage(img, 0,0, this);
             for (Line line : lines) {
@@ -75,6 +83,7 @@ public class LinesComponent extends JComponent{
                 g2.setColor(Color.red);
                 g2.setStroke(new BasicStroke(line.stroke));
                 g2.draw(new Line2D.Double(line.x1, line.y1, line.x2, line.y2));
+                g2.draw(new Line2D.Double(line.x1, line.y1, line.x3, line.y3));
                 //g.drawLine(line.x1, line.y1, line.x2, line.y2);
             }
         }catch (IOException e){
